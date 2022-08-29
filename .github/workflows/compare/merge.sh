9660e7247b9e7c831c8e138092625a058cc1f183
sungamma <sungamma@gmail.com>
2022-08-29 16:56:04 +0800

    整理目录，隐藏部分yml

--- .github/workflows/compare/merge.sh
+++ .github/workflows/compare/merge.sh
@@ -2,6 +2,7 @@ diff build.yml compare/build.yml.bak > /dev/null
 if [  "$?" == "0" ]; then #"$?"是上一执行命令的返回值。
     echo "nothing to change"
     else
+    sudo snap install yq
     cp build.yml build-2.yml #制作实时更新的带有修改记录的action文件
     cp build.yml compare/build.yml.bak #为下次比较否有变化做准备
     myenv=$(cat compare/tags-build.txt) yq -i '.jobs.create-release=env(myenv)' build-2.yml  # 把生成修改记录的action放入build-2.yml
